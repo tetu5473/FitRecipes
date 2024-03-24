f4b62d8eb58a69576fbe750703d591506a1a8edf
tetsuya oota <tetu5473@gmail.com>
2024-03-24 21:46:37 +0000

    0325

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,3 @@ User.create!(name: "Sample User",
              password_confirmation: "password",
              admin: true)
 
-<<<<<<< HEAD
-60.times do |n|
-  name  = Faker::Name.name
-  email = "sample-#{n+1}@email.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
-=======
->>>>>>> japanization-and-time-format
