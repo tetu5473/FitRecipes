8a7fb0d96d8c51e1d1679c4753b428701ea3ed01
tetsuya oota <tetu5473@gmail.com>
2024-03-24 09:27:47 +0000

    終わり

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2023_12_17_110704) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_120807) do
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "calories"
-    t.integer "prep_time"
-    t.integer "cook_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.text "ingredients"
-    t.text "instructions"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-
->>>>>>> japanization-and-time-format
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
